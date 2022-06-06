5dee2f62bb733a6d7e75b8fd540c35e4cf55a492
luis.freire <luis.freire@academico.ifpb.edu.br>
2022-06-06 16:57:35 -0300

    commit5 - Adicionar algoritmo de inserção no Banco e converter graus para radianos

--- modules/calculos/distanciaPercorridaSatelite.py
+++ modules/calculos/distanciaPercorridaSatelite.py
@@ -1,6 +1,14 @@
 import math
 import mpmath
 
+def converterLongitudeToRad(lng):
+    lng_rad = (3,141593 * lng) / 180
+    return lng_rad
+
+def converterLatitudeToRad(lat):
+    lat_rad = (1.570796 * lat ) / 20
+    return lat_rad
+
 def obterVariacaoLatLng(a,b):
     deltaA = a
     deltaB = b
@@ -15,7 +23,6 @@ def obterVariacaoLatLng(a,b):
         return deltaB - deltaA
 
 def calcularDistanciaPercorrida(lat1,lat2,lng1,lng2):
-    
     lx1 = ((math.sin(obterVariacaoLatLng(lat1,lat2))/2)**2) * math.cos(lat2) * (math.sin(obterVariacaoLatLng(lng1,lng2))/2)
     lx2 = 2* mpmath.cot(math.sqrt(lx1) / math.sqrt(1-lx1) )
     RaioTerra = 6.371 # Km
--- modules/sql/sqlManager.py
+++ modules/sql/sqlManager.py
@@ -1,6 +1,6 @@
 import sqlite3
-
-
+from log.Log import Log
+from calculos.distanciaPercorridaSatelite import calcularDistanciaPercorrida
 conectarDB_Satelite = sqlite3.connect("SateliteISS.db")
 
 cursorSatelite = conectarDB_Satelite.cursor()
@@ -10,7 +10,8 @@ cursorSatelite.execute("""
                            ID: INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                            DATAHORA: DATETIME NOT NULL
                            LATITUDE: REAL NOT NULL,
-                           LONGITUDE: REAL NOT NULL
+                           LONGITUDE: REAL NOT NULL,
+                           
                        )
                        """)
 
@@ -19,12 +20,19 @@ cursorSatelite.execute("""
 def inserirNovoLog(log):
     
     cursorSatelite.execute("""
+                                    SELECT * FROM SateliteLog ORDER by ID DESC LIMIT 1;
+                           """)
+    lastLog = cursorSatelite.fetchone()
+    distPercorrida = calcularDistanciaPercorrida(lastLog[2],log.lat,lastLog[3],log.l)
+    new_log = Log(log.dataHora,log.lat,log.long,distPercorrida)
+    cursorSatelite.execute("""
                                     
-                                    INSERT INTO SateliteLog(DATAHORA,LATITUDE,LONGITUDE) values (?,?,?)
+                                    INSERT INTO SateliteLog(DATAHORA,LATITUDE,LONGITUDE) values (?,?,?);
                                     
                                     
                                     """,
-                                    (log.datahora,log.lat,log.lng)
+                                    (new_log.datahora,new_log.lat,new_log.lng,new_log.distanciaPercorrida)
     )
+        
     conectarDB_Satelite.commit()
     return
