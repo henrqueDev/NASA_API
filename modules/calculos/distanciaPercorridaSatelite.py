9de1fa1f6d45816875b79d1d2d0f1879e02b0bc9
luis.freire <luis.freire@academico.ifpb.edu.br>
2022-06-09 16:12:22 -0300

    Calculo 100% funcional

--- modules/calculos/distanciaPercorridaSatelite.py
+++ modules/calculos/distanciaPercorridaSatelite.py
@@ -18,30 +18,11 @@ def obterVariacaoLatLng(a,b):
 def calcularDistanciaPercorrida(lat1,lat2,lng1,lng2):
     lx1 = (((math.sin(obterVariacaoLatLng(lat1,lat2))/2)**2) + math.cos(lat1)) * math.cos(lat2) * ((math.sin(obterVariacaoLatLng(lng1,lng2)/2)) ** 2)
     try :
-        print(lx1)
-        lx3 = mpmath.cot(math.sqrt(0.41666) / math.sqrt(1-0.41666) )
-        print(lx3)
+        lx3 = mpmath.atan(math.sqrt(lx1) / math.sqrt(1-lx1) )
         lx2 = 2 * lx3
     except ZeroDivisionError:
         lx2 = 0
-    print(lx2)
     RaioTerra = 6.371 # Km
     distanciaPercorrida = RaioTerra * lx2
     
     return distanciaPercorrida
-
-
-
-a = converterLatLongToRad(34)
-b = converterLatLongToRad(35)
-c = converterLatLongToRad(118)
-d = converterLatLongToRad(139)
-
-n = calcularDistanciaPercorrida(0.5942 ,0.6227,-2.0634,2.4387)
-print(n)
-
-"""
-
-SUBSTITUIR COT POR ARCO TANGENTE
-
-"""
