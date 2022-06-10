ca1b7096208962d41401f677089ded93b622d9b8
luis.freire <luis.freire@academico.ifpb.edu.br>
2022-06-10 16:06:36 -0300

    Ajeitar neogcio

--- modules/calculos/distanciaPercorridaSatelite.py
+++ modules/calculos/distanciaPercorridaSatelite.py
@@ -1,11 +1,11 @@
 import math
 import mpmath
 
-def converterLatLongToRad(latLong):
+def __converterLatLongToRad(latLong):
     latLong_rad = latLong * (3.141593 / 180)
     return latLong_rad
 
-def obterVariacaoLatLng(a,b):
+def __obterVariacaoLatLng(a,b):
     deltaA = a
     deltaB = b
     variacao = (deltaA - deltaB)
@@ -13,7 +13,7 @@ def obterVariacaoLatLng(a,b):
         variacao = (deltaA - deltaB) * -1
     return variacao
 def calcularDistanciaPercorrida(lat1,lat2,lng1,lng2):
-    lx1 = (((math.sin(obterVariacaoLatLng(lat1,lat2))/2)**2) + math.cos(lat1)) * math.cos(lat2) * ((math.sin(obterVariacaoLatLng(lng1,lng2)/2)) ** 2)
+    lx1 = (((math.sin(__obterVariacaoLatLng(lat1,lat2))/2)**2) + math.cos(lat1)) * math.cos(lat2) * ((math.sin(__obterVariacaoLatLng(lng1,lng2)/2)) ** 2)
     try :
         lx3 = mpmath.atan(math.sqrt(lx1) / math.sqrt(1-lx1) )
         lx2 = 2 * lx3
