58a53d543df6fb478f23905ebfb1d7d181b17a0c
luis.freire <luis.freire@academico.ifpb.edu.br>
2022-06-10 15:26:31 -0300

    tirar print inutil

--- modules/calculos/distanciaPercorridaSatelite.py
+++ modules/calculos/distanciaPercorridaSatelite.py
@@ -10,10 +10,7 @@ def obterVariacaoLatLng(a,b):
     deltaB = b
     variacao = (deltaA - deltaB)
     if(variacao < 0):
-        #print('deltaA to positive')
         variacao = (deltaA - deltaB) * -1
-    
-    print(variacao)
     return variacao
 def calcularDistanciaPercorrida(lat1,lat2,lng1,lng2):
     lx1 = (((math.sin(obterVariacaoLatLng(lat1,lat2))/2)**2) + math.cos(lat1)) * math.cos(lat2) * ((math.sin(obterVariacaoLatLng(lng1,lng2)/2)) ** 2)
